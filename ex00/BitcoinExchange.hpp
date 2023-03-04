f011b96174cf33b677ec1c99f18dfeb1feaaa814
Miguel Villaescusa <75221782+mikevgb@users.noreply.github.com>
2023-03-04 15:05:00 +0100

    basic parse in ex00

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,86 @@
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+#include <sstream>
+
+class BitcoinExchange
+{
+    private:
+    std::map<std::string, int> _csvdata;
+    void printError(std::string message)
+    {
+        std::cerr << message << std::endl;
+        exit(1);
+    };
+
+    public:
+    //!! implementation in hpp file is forbidden!!!
+    BitcoinExchange(std::string fileInput) 
+    {
+        readDatabaseCSV();
+    };
+    ~BitcoinExchange() {};
+    //!! canonical
+    void readDatabaseCSV()
+    {
+        std::ifstream file("data.csv");
+        if (file.is_open())
+        {
+            std::string line;
+            while (std::getline(file, line))
+            {
+                std::stringstream ss(line);
+                std::string date, value;
+                std::getline(ss, date, ',');
+                std::getline(ss, value, ',');
+                int intval = std::stoi(value);
+                parseDate(date);
+                _csvdata[date] = intval;
+            }
+            file.close();
+        }
+        else
+            printError("Error: cant open data.csv");
+    };
+    void readFileInput(std::string fileName)
+    {
+                std::ifstream file(fileName);
+        if (file.is_open())
+        {
+            std::string line;
+            while (std::getline(file, line))
+            {
+                std::stringstream ss(line);
+                std::string date, value;
+                std::getline(ss, date, '|');
+                std::getline(ss, value, '|');
+                int intval = std::stoi(value);
+                parseDate(date);
+                _csvdata[date] = intval;
+            }
+            file.close();
+        }
+        else
+            printError("Error: cant open input file");
+    };
+    void parseDate(std::string date)
+    {
+        int year = std::atoi(date.substr(0,4));
+        int month = std::atoi(date.substr(5,2));
+        int day = std::atoi(date.substr(8,2));
+        if (year < 2009 || year > 2022)
+            printError("Error: year out of range.");
+        if (month < 1 || month > 12)
+           printError("Error: month out of range.");
+        if (day < 1 || day > 31)
+            printError("Error: day out of range");
+        if (month == 2 && day > 29)
+            printError("Error: day out of range");        
+    };
+};
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,11 @@
+#include "BitcoinExchange.hpp"
+
+int main(int argc, char **argv)
+{
+    if (argc < 2 || argc > 3)
+    {
+        std::cout << "Wrong number of arguments provided." << std::endl;
+        return 0;
+    }
+
+}
