277ab69f72f1760a66d4880a2ef929c33dd791e6
Miguel Villaescusa <75221782+mikevgb@users.noreply.github.com>
2023-03-10 13:55:32 +0100

    trouble with getline reading txt with delimeters

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -11,10 +11,10 @@ class BitcoinExchange
 {
     private:
     //!! value must be either a float or a positive int
-    std::map<std::string, int> _csvData;
-    std::map<std::string, int> _inputData;
-    std::map<std::string, int> _result;
-    BitcoinExchange() {};
+    std::map<std::string, std::string> _csvData;
+    std::map<std::string, std::string> _inputData;
+    //std::map<std::string, int> _result;
+    
     void printError(std::string message)
     {
         std::cerr << message << std::endl;
@@ -22,10 +22,21 @@ class BitcoinExchange
     }
 
     public:
+    BitcoinExchange() 
+    {
+        readDatabaseCSV();
+        //printMaps();
+        readFileInput();
+        printMaps();
+        //multiValues();
+
+    };
     //!! implementation in hpp file is forbidden!!!
     BitcoinExchange(std::string fileInput) 
     {
+        (void)fileInput;
         readDatabaseCSV();
+        readFileInput();
     };
     ~BitcoinExchange() {};
     //!! canonical
@@ -41,17 +52,18 @@ class BitcoinExchange
                 std::string date, value;
                 std::getline(ss, date, ',');
                 std::getline(ss, value, ',');
-                int intval = std::stoi(value);
-                _csvData[date] = intval;
+                _csvData[date] = value;
             }
             file.close();
         }
         else
             printError("Error: could not open data.csv");
     };
-    void readFileInput(std::string fileName)
+
+    //void readFileInput(std::string fileName)
+    void readFileInput()
     {
-        std::ifstream file(fileName);
+        std::ifstream file("input.txt");
         if (file.is_open())
         {
             std::string line;
@@ -61,37 +73,62 @@ class BitcoinExchange
                 std::string date, value;
                 std::getline(ss, date, '|');
                 std::getline(ss, value, '|');
-                int intval = std::stoi(value);
-                if (intval < 0 || intval > 1000)
-                    std::cerr << "Error: too large number." << std::endl;
-                if (!parseDate(date) && intval > 0 || intval <= 1000)
-                    _inputData[date] = intval;
+                _inputData[date] = value;
             }
             file.close();
         }
         else
             printError("Error: could not open file.");
     };
-    int parseDate(std::string date)
+
+    void parseDate(std::string date)
     {
         int year = std::stoi(date.substr(0,4));
         int month = std::stoi(date.substr(5,2));
         int day = std::stoi(date.substr(8,2));
         if (year < 2009 || year > 2022 || month < 1 || month > 12 || day < 1 || day > 31)
-        {
             std::cout << "Error: bad input" << " => " << date << std::endl;
-            return(1);
-        }
         if (month == 2 && day > 29)
-        {
             std::cout << "Error: bad input" << " => " << date << std::endl; 
-            return(1);
-        }
-        return(0);     
     };
+
+    void printMaps()
+    {
+        // std::map<std::string, std::string>::const_iterator it;
+        // for(it = _csvData.begin(); it != _csvData.end(); it++)
+        //     std::cout << it->first << " " << it->second << std::endl;
+        std::map<std::string, std::string>::const_iterator it2;
+        for(it2 = _inputData.begin(); it2 != _inputData.end(); it2++)
+            std::cout << it2->first << " " << it2->second << std::endl;
+    };
+
     void multiValues()
     {
-        
+        std::map<std::string, std::string>::const_iterator it;
+        std::map<std::string, std::string>::const_iterator it2;
+        it2 = _inputData.begin();
+        for (it = _csvData.begin(); it != _csvData.end(); it++)
+        {
+            for (it2 = _inputData.begin(); it2 != _inputData.end(); it2++)
+            {
+                if (it->first == it2->first && it->first != "date" && it2->first != "date")
+                {
+                    // std::cout << "hey!" << std::endl;
+                    float tmp1;
+                    float tmp2;
+                    float result = 0.0;
+                    tmp1 = std::stof(_csvData.at(it->second));
+                    tmp2 = std::stof(_inputData.at(it2->second));
+                    //if (tmp2 > 0)
+                    //{
+                        result = tmp1 * tmp2;
+                        std::cout << it->first << " => " << it2->second << " " << result << std::endl;
+                    //}
+                    if (tmp2 > 0)
+                        std::cout << "Error: not a positive number." << std::endl;
+                }
+            }
+        }
     };
 };
 
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,15 +1,20 @@
 #include "BitcoinExchange.hpp"
 
-int main(int argc, char **argv)
+int main()
 {
-    if (argc < 2)
-    {
-        std::cerr << "Error: could not open file." << std::endl;
-        return 1;
-    }
-    if (argc > 3)
-    {
-        std::cerr << "Error: Too many arguments." << std::endl;
-        return 1;
-    }
+    // if (argc < 2)
+    // {
+    //     std::cerr << "Error: could not open file." << std::endl;
+    //     return 1;
+    // }
+    // else if (argc > 3)
+    // {
+    //     std::cerr << "Error: Too many arguments." << std::endl;
+    //     return 1;
+    // }
+    // else
+    //     BitcoinExchange btx(std::string(argv[1]));
+    BitcoinExchange btx;
+
+    return 0;
 }
