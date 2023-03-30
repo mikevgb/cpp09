10e117b368463a97e8b12e359a994ee5ad1e7f8e
mikevgb <75221782+mikevgb@users.noreply.github.com>
2023-03-30 12:50:40 +0200

    simple mergeinsertion with vector and list

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -3,127 +3,127 @@
 
 #include <iostream>
 #include <vector>
-#include <utility>
-#include <sstream>
 #include <algorithm>
+// #include <sstream>
+#include <cstring>
+#include <list>
+#include <limits>
 
 class PmergeMe
 {
-    std::string _input;
-    std::vector<std::pair<int, int> > _pairV;
+    private:
+    std::vector<int> _vector;
+    std::list<int> _list;
+    std::vector<int>::iterator it;
 
     public:
+    void printBadArgument(int code)
+    {
+        std::cerr << "Error: bad argument " << code << std::endl;
+        exit(1);
+    };
+    void parseAndStore(char *argv)
+    {
+        int len = strlen(argv);
+        for (int i = 0; i < len; i++)
+            if (!isdigit(argv[i])) //check if all chars are numbers
+                printBadArgument(1);
+        char *end;
+        long int tmp = strtol(argv, &end, 10);
+        if (tmp > std::numeric_limits<int>::max()) //check if int range
+            printBadArgument(2);
+        it = std::find(_vector.begin(), _vector.end(), tmp); //check repeated
+        if (it != _vector.end())
+            printBadArgument(3);
+        _vector.push_back(tmp); //store numbers
+        _list.push_back(tmp);
+    };
     PmergeMe(int argc, char **argv)
     {
-        //!!!measure time
-        
-        vectorOfPairs(argc, argv);
+        int tmp;
+        for (int i = 1; i < argc; i++)
+            parseAndStore(argv[i]);
+
+        //printVector();
+        clock_t startTime = clock();
+        mergeInsertionSortVector(_vector);
+        double elapsedTime = (clock() - startTime) / (double)CLOCKS_PER_SEC;
+        std::cout << "Time vector = " << elapsedTime << std::endl;
+        startTime = clock();
+        mergeInsertionSortList(_list);
+        elapsedTime = (clock() - startTime) / (double)CLOCKS_PER_SEC;
+        std::cout << "Time list = " << elapsedTime << std::endl;
+        //printVector();
     };
     ~PmergeMe() {};
-    void vectorOfPairs(int argc, char **argv)
+    void mergeInsertionSortVector(std::vector<int> &input) 
     {
-        //!!! check if number of elements its odd or even
-        //4 even
-        std::cout << "Before: ";
-        for (int i = 1; i < argc; i++)
+        const int threshold = 16;
+        if (input.size() <= threshold)
         {
-            std::cout << argv[i] << " ";
+            std::sort(input.begin(), input.end());
+            return;
         }
-        std::cout << std::endl;
-
-        for (int i = 1; i < argc; i+=2)
+        std::vector<int> left(input.begin(), input.begin() + input.size() / 2);
+        std::vector<int> right(input.begin() + input.size() / 2, input.end());
+        mergeInsertionSortVector(left);
+        mergeInsertionSortVector(right);
+        input.clear();
+        std::merge(left.begin(), left.end(), right.begin(), right.end(), std::back_inserter(input));
+        if (input.size() > threshold)
         {
-            //make sorted pairs
-            int tmp1;
-            int tmp2;
-            std::stringstream ss(argv[i]);
-            ss >> tmp1;
-            std::stringstream ss2(argv[i + 1]);
-            ss2 >> tmp2;
-            if (tmp1 < tmp2)
-                _pairV.push_back(std::make_pair(tmp1, tmp2));
-            else
-                _pairV.push_back(std::make_pair(tmp2, tmp1));
-            
+            std::vector<int>::iterator it = input.begin() + threshold;
+            std::sort(it, input.end());
         }
-        //sort based on A
-        std::sort(_pairV.begin(), _pairV.end());
-        //call binaryInsertion till end
-        int mid;
-        
-        for(size_t i = 0; i < _pairV.size(); i++)
-        {
-            
-            if (_pairV[i].second != -1)
-            {
-                mid = _pairV.size() / 2;
-                //printVectorOfPairs();
-                //std::cout << "mid send = " << mid << std::endl;
-                //std::cout << "valToIndex = " << _pairV[i].second << std::endl;
-                binarySearchInsertionVectorOfPairs(mid, _pairV[i].second);
-                _pairV[i].second = -1;
-                //std::cout << "------------------------" << std::endl;
-            }
-        }
-        std::cout << "After:  ";
-        for (size_t i = 0; i < _pairV.size(); i++)
-        {
-            std::cout << _pairV[i].first << " ";
-        }
-        std::cout << std::endl;
-        //printVectorOfPairs();
     };
-
-
-    int recalcMidVectorOfPairs(int lastMid)
+    void mergeInsertionSortList(std::list<int>& input)
     {
-        int idk = ((_pairV.size() - lastMid) / 2);
-        return _pairV.size() / 2 + idk;
-    };
-
-    void binarySearchInsertionVectorOfPairs(int lastMid, int valToIndex)
-    {
-        int recalcMid;
-        if (valToIndex > _pairV[lastMid].first)
+        const int threshold = 16;
+        if (input.size() <= threshold) 
         {
-            if (valToIndex < _pairV[lastMid + 1].first)
-                _pairV.insert(_pairV.begin() + lastMid + 1, std::make_pair(valToIndex, -1));
-            else if (valToIndex > _pairV[_pairV.size() - 1].first)
-                _pairV.push_back(std::make_pair(valToIndex, -1));
-            else
-            {
-                recalcMid = recalcMidVectorOfPairs(lastMid);
-                binarySearchInsertionVectorOfPairs(recalcMid, valToIndex);
-            }      
+            input.sort();
+            return;
         }
-        else if (valToIndex < _pairV[lastMid].first)
+        std::list<int> left, right;
+        std::list<int>::iterator it = input.begin();
+        for (int i = 0; i < input.size() / 2; i++) 
         {
-            if (valToIndex > _pairV[lastMid + 1].first)
-                _pairV.insert(_pairV.begin() + lastMid - 1, std::make_pair(valToIndex, -1));
-            else
-            {
-                recalcMid = recalcMidVectorOfPairs(lastMid);
-                binarySearchInsertionVectorOfPairs(recalcMid, valToIndex);
-            }
+            left.push_back(*it);
+            it++;
+        }
+        while (it != input.end()) 
+        {
+            right.push_back(*it);
+            it++;
+        }
+        mergeInsertionSortList(left);
+        mergeInsertionSortList(right);
+        input.clear();
+        std::merge(left.begin(), left.end(), right.begin(), right.end(), std::back_inserter(input));
+        if (input.size() > threshold) 
+        {
+            std::list<int>::iterator it = input.begin();
+            std::advance(it, threshold);
+            std::list<int> remaining(it, input.end());
+            remaining.sort();
+            input.erase(it, input.end());
+            input.merge(remaining);
         }
     };
 
-    void printVectorOfPairs()
+    void printVector()
     {
-        for(size_t i = 0; i < _pairV.size(); i++)
-        {
-            std::cout << _pairV[i].first << " " << _pairV[i].second << std::endl;
-        }
-    };
+        std::cout << "print vector" << std::endl;
+        //std::cout << _vectorGroupCounter << std::endl;
+        for (int i = 0; i < _vector.size(); i++)
+            std::cout << i << " = " << _vector[i] << std::endl;
+    }
+
 };
 
+//make groups of 15 elements
+//insertion sort https://iq.opengenus.org/insertion-sort/
+
+//./PmergeMe `shuf -i 1-100000 -n 3000 | tr "\n" " "`
+
 #endif
-
-
-//1. group list into pairs, if list has odd number of elements, the last one is unpaired
-
-//2. sort each pair (comparing A and B)
-
-//3. order the list based on A, the unpaired is consider the last B
-
-//4. binary insertion sort of group B on A
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,24 +2,11 @@
 
 int main(int argc, char **argv)
 {
-    if (argc > 2)
-    {
-        /*
-        std::string arg1;
-        for (int i = 1; i < argc; i++)
-        {
-            
-            arg1.append(argv[i]);
-            arg1.append(" ");
-            
-        }
-        */
-       
-        //std::cout << "Input " << argv << std::endl;
-        PmergeMe shorty(argc, argv);
-        //shorty.printVectorOfPairs();
-        // shorty.printInput();
-        // shorty.printVector();
-    }
+    PmergeMe PM(argc, argv);
+    //PM.printVector();
+    //PM.makeGroupsAndSortVector();
+    //PM.printVector();
+    
+
     return 0;
 }
